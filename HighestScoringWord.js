161bbb2f93e0d7df814ca1d5bd3e6d2ea085efef
Wesley Wardlaw <wesley.ryan.wardlaw@gmail.com>
2019-07-10 18:52:08 -0500

    add HighestScoringWord

---
+++ HighestScoringWord.js
@@ -0,0 +1,50 @@
+// Given a string of words, you need to find the highest scoring word.
+
+// Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+// You need to return the highest scoring word as a string.
+
+// If two words score the same, return the word that appears earliest in the original string.
+
+// All letters will be lowercase and all inputs will be valid.
+
+function high(x) {
+    let key = {
+        a: 1,
+        b: 2,
+        c: 3,
+        d: 4,
+        e: 5,
+        f: 6,
+        g: 7,
+        h: 8,
+        i: 9,
+        j: 10,
+        k: 11,
+        l: 12,
+        m: 13,
+        n: 14,
+        o: 15,
+        p: 16,
+        q: 17,
+        r: 18,
+        s: 19,
+        t: 20,
+        u: 21,
+        v: 22,
+        w: 23,
+        x: 24,
+        y: 25,
+        z: 26
+    };
+    let words = x.split(' ');
+    let counts = words.map(word => {
+        let count = 0;
+        for (let i = 0; i < word.length; i++) {
+            count += key[word[i]];
+        }
+        return count;
+    });
+
+    return words[counts.indexOf(Math.max(...counts))];
+}
